ac6fda35b4b6522489f9434e8927094626755cd0
mac-012 <lolakreet@gmail.com>
2020-12-08 10:19:16 +0900

    update

--- app/src/main/java/com/example/demoexam/SignIn.java
+++ app/src/main/java/com/example/demoexam/SignIn.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class SignIn extends AppCompatActivity {
 
@@ -12,12 +14,13 @@ public class SignIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signin);
-
-
     }
 
-    public void onClickReg(View r){
-        Intent intentReg = new Intent(SignIn.this, SignUp.class);
-        startActivity(intentReg);
+    public void onClickLogin(View r){
+        //Intent intentReg = new Intent(SignIn.this, SignUp.class);
+        //startActivity(intentReg);
+
+        TextView log = findViewById(R.id.Login); String logInput = (String) log.getText().toString(); Log.d("log",logInput);
+        TextView pass = findViewById(R.id.Password); String passInput = (String) pass.getText().toString(); Log.d("pass",passInput);
     }
 }
